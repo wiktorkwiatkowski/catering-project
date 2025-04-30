456790b37a847f35e4d2427346947f5e0f844968
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2025-04-30 21:26:49 +0200

    formatting, comments

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, jsonify, render_template
 import mysql.connector
 
+# Stworzenie obiektu klasy Flash, naszego "serwera"
 app = Flask(__name__)
 
+
+# Funkcja do łączenia się z bazą danych "catering" z użytkownikiem i hasłem root
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='root',  
-        database='catering'
+        host="localhost", user="root", password="root", database="catering"
     )
 
-@app.route('/menu')
+
+# Gdy ktoś wejdzie w /menu to aplikacja łączy się z bazą danych i pobiera rekordy
+# z tabeli menu i zwraca je jako json
+@app.route("/menu")
 def menu():
     conn = get_db_connection()
     cursor = conn.cursor(dictionary=True)
@@ -20,9 +23,17 @@ def menu():
     conn.close()
     return jsonify(items)
 
+
+# Główna strona /, Flask ładuje szablon templates/index.html i zwraca go jako stronę HTML
 @app.route('/')
 def index():
-    return render_template("index.html")
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM menu")
+    items = cursor.fetchall()
+    conn.close()
+    return render_template("index.html", menu=items)
 
-if __name__ == '__main__':
+# Start aplikacji
+if __name__ == "__main__":
     app.run(debug=True)
